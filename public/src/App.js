dfb982c1c98ce059f12d5ad0d6d20ab0add0eff7
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-08-23 00:05:12 +0200

    Installed package and components

--- public/src/App.js
+++ public/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Secret from './pages/Secret';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+     <Routes>
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/" element={<Secret />} />
+     </Routes>
+    </BrowserRouter>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.listen(4000, () => {
     console.log("Server started on port 4000");
 });
+
+mongoose.connect("mongodb://localhost:27017/jwt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("DB Connection Successfull");
+}).catch(err => {
+    console.log(err.message);
+})
 
 app.use(
     cors({
@@ -13,3 +25,5 @@ app.use(
         credentials: true,
     })
 );
+
+app.use(express.json());
