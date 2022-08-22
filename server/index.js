ffe83d95dff8ac2c6cda4abcc9dc88e0b761feda
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-08-22 23:30:52 +0200

    Installed server and client

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.listen(4000, () => {
+    console.log("Server started on port 4000");
+});
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        method: ["GET", "POST"],
+        credentials: true,
+    })
+);
