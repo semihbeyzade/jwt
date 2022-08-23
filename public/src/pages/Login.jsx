d7a1cea48253f482448988af1e60bd27cb83224a
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-08-23 10:15:08 +0200

    Created register login component

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from "react-router-dom";
+import {ToastContainer, toast} from "react-toastify";
+import axios from "axios";
 
 export default function Login() {
+
+  const [values, setValues] = useState({
+      email: "",
+      password: "",
+  });
+
+   const handleSubmit = async (e) => {
+       e.preventDefault();
+       try {
+         const {data} = await axios.post("http://localhost:4000/register", {
+             ...values, 
+         });
+       } catch(err) {
+           console.log(err);
+       }
+   }
+
   return (
-    <div>Login</div>
+    <div className='container'>
+        <h2>Login Account</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <div>
+                <label htmlFor='email'>Email</label>
+                <input 
+                type="email" 
+                name='email' 
+                placeholder='Email' 
+                onChange={(e) => 
+                setValues({...values, [e.target.name]: e.target.value})} />
+            </div>
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input 
+                type="password" 
+                name='password' 
+                placeholder='Password'
+                onChange={(e) => 
+                setValues({...values, [e.target.name]: e.target.value})} />
+            </div>
+            <button type='submit'>Submit</button>
+            <span>
+                Already have an account? <Link to="/register">Register</Link>
+            </span>
+        </form>
+        <ToastContainer />
+    </div>
   )
 }
--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom";
 import {ToastContainer, toast} from "react-toastify";
+import axios from "axios";
 
 export default function Register() {
+
+  const [values, setValues] = useState({
+      email: "",
+      password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const {data} = await axios.post("http://localhost:4000/register", {
+          ...values, 
+      });
+    } catch(err) {
+        console.log(err);
+    }
+}
+
   return (
     <div className='container'>
         <h2>Register Account</h2>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <div>
                 <label htmlFor='email'>Email</label>
-                <input type="email" name='email' placeholder='Email' />
+                <input 
+                type="email" 
+                name='email' 
+                placeholder='Email' 
+                onChange={(e) => 
+                setValues({...values, [e.target.name]: e.target.value})} />
             </div>
             <div>
                 <label htmlFor='password'>Password</label>
-                <input type="password" name='password' placeholder='Password' />
+                <input 
+                type="password" 
+                name='password' 
+                placeholder='Password'
+                onChange={(e) => 
+                setValues({...values, [e.target.name]: e.target.value})} />
             </div>
             <button type='submit'>Submit</button>
             <span>
-                Already have ab account? <Link to="/login">Login</Link>
+                Already have an account? <Link to="/login">Login</Link>
             </span>
         </form>
         <ToastContainer />
