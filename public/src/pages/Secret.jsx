58e532e6828853e4c6af426a3f5a2c3b6556b1b4
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-09-03 10:04:33 +0200

    completed project

--- public/src/pages/Secret.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Secret() {
-  return (
-    <div>Secret</div>
-  )
-}
--- server/index.js
+++ server/index.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authRoutes = require("./Routes/AuthRoutes");
-
-const app = express();
+const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 
-app.listen(4000, () => {
-    console.log("Server started on port 4000");
+const app = express();
+
+app.listen(4000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
 });
 
-mongoose.connect("mongodb://localhost:27017/jwt", {
+mongoose
+  .connect("mongodb://localhost:27017/jwt", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
-    console.log("DB Connection Successfull");
-}).catch(err => {
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
     console.log(err.message);
-})
+  });
 
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        method: ["GET", "POST"],
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
 );
-
 app.use(cookieParser());
+
 app.use(express.json());
 app.use("/", authRoutes);
